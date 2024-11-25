cab44da20014899e0bccdf24b5a0f40aee928051
unknown <topervillablanca@gmail.com>
2024-11-25 13:58:12 +0800

    Initial commit

---
+++ c2-backend/c2-backend/src/main/java/com/topervillablanca/c2/c2_backend/C2BackendApplication.java
@@ -0,0 +1,13 @@
+package com.topervillablanca.c2.c2_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class C2BackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(C2BackendApplication.class, args);
+	}
+
+}
---
+++ c2-backend/c2-backend/src/test/java/com/topervillablanca/c2/c2_backend/C2BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.topervillablanca.c2.c2_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class C2BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
